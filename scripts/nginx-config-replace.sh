ccf825f7273bf2d7eb64bdc1fb60e4ddfedea99e
Walter Scarborough <wscarbor@tacc.utexas.edu>
2015-05-22 10:39:47 -0500

    Reorganized env vars

--- scripts/nginx-config-replace.sh
+++ scripts/nginx-config-replace.sh
@@ -2,11 +2,11 @@
 # Based on: https://github.com/jacksoncage/nginx-docker
 
 # Using environment variables to set nginx configuration
-[ -z "${NGINX_SERVER_NAME}" ] && echo "\$NGINX_SERVER_NAME is not set" || sed -i "s:NGINX_SERVER_NAME:${NGINX_SERVER_NAME}:" /etc/nginx/nginx.conf
+[ -z "${SERVER_NAME}" ] && echo "\$SERVER_NAME is not set" || sed -i "s:SERVER_NAME:${SERVER_NAME}:" /etc/nginx/nginx.conf
 
-[ -z "${NGINX_SSL_BUNDLE_PATH}" ] && echo "\$NGINX_SSL_BUNDLE_PATH is not set" || sed -i "s:NGINX_SSL_BUNDLE_PATH:${NGINX_SSL_BUNDLE_PATH}:" /etc/nginx/nginx.conf
+[ -z "${SSL_BUNDLE_PATH}" ] && echo "\$SSL_BUNDLE_PATH is not set" || sed -i "s:SSL_BUNDLE_PATH:${SSL_BUNDLE_PATH}:" /etc/nginx/nginx.conf
 
-[ -z "${NGINX_SSL_KEY_PATH}" ] && echo "\$NGINX_SSL_KEY_PATH is not set" || sed -i "s:NGINX_SSL_KEY_PATH:${NGINX_SSL_KEY_PATH}:" /etc/nginx/nginx.conf
+[ -z "${SSL_KEY_PATH}" ] && echo "\$SSL_KEY_PATH is not set" || sed -i "s:SSL_KEY_PATH:${SSL_KEY_PATH}:" /etc/nginx/nginx.conf
 
 [ -z "${VDJAPI_PORT_8443_TCP_ADDR}" ] && echo "\$VDJAPI_PORT_8443_TCP_ADDR is not set" || sed -i "s:VDJAPI_PORT_8443_TCP_ADDR:${VDJAPI_PORT_8443_TCP_ADDR}:" /etc/nginx/nginx.conf
 
